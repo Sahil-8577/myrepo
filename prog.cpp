635eff4e77eac47d1bd2e98f2d084287e9188a3d
sahil <sahilbarwal0786@gmail.com>
2025-03-20 23:48:29 +0530

    prog.cpp added

---
+++ prog.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ bubble sort(){
+	std::cout << "bubble sort" << std::endl;
+}
+
+quicksort(){
+ std::cout << "quick sort method added" << std::endl;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
